0f4fffa63995ad201b2a319b127097c1551bf616
Gustavo Nascimento <gustavo.nascprojetos7@gmail.com>
2025-08-01 16:03:06 -0300

    Add o Jest

---
+++ tests/sample.test.js
@@ -0,0 +1,7 @@
+// Este é um teste de exemplo para garantir que o Jest está funcionando.
+// Ele não testa a API de vdd, apenas valida o workflow.
+describe('Ambiente de Teste', () => {
+  it('deve retornar true se 1 + 1 for igual a 2', () => {
+    expect(1 + 1).toBe(2);
+  });
+});
