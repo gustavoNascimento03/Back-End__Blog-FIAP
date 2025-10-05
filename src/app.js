24f20ca9ee5d929baeb33841a1ef1e3c8c038029
Gustavo Nascimento <gustavo.nascprojetos7@gmail.com>
2025-10-05 18:34:59 -0300

    Add Cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const postRoutes = require('./routes/postRoutes');
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
